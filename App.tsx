66efcb70020e2e92ec0ab9834e12a03cd6d914e5
Bearyeiz <giaphuoc.work@gmail.com>
2024-08-15 00:25:01 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import DetailScreen from './src/screens/DetailScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen 
+          name="Tab" 
+          component={TabNavigator} 
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+
+        <Stack.Screen 
+          name="Detail" 
+          component={DetailScreen} 
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+
+      <Stack.Screen 
+          name="Payment" 
+          component={PaymentScreen} 
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
+
